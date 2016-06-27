776db504c0c0dd756fa2282fd8a579f3d5912955
gun6 <gunther_vlerick@hotmail.com>
2016-06-27 11:56:43 +0200

    allemaal

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     
     public static void main(String[] args) {
-        System.out.println("hallo iedereen");
+        System.out.println("hallo allemaal");
     }
     
 }
